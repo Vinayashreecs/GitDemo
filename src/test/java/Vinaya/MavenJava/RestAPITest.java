799393d7ffb9b22fc51bb540a411a1a1efd83ba8
Vinaya <vinaya@gmail.com>
2021-02-05 21:23:20 +0530

    Committed

---
+++ src/test/java/Vinaya/MavenJava/RestAPITest.java
@@ -0,0 +1,21 @@
+package Vinaya.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void PostJira() {
+		
+		System.out.println("PostJira");
+
+	}
+
+	@Test
+	public void DeleteTwitter() {
+
+		System.out.println("DeleteTwitter");
+
+	}
+
+}
